a3aa37a1f8e2cd0ad254df67e972f5c356358602
Sergey <Sergey@Sergeys-MacBook-Pro-2.local>
2016-11-30 13:26:46 +0200

    Add input and output filename parameters

---
+++ Parse_link.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+import csv
+import urllib
+import urllib2
+import json
+import StringIO
+from StringIO import StringIO
+import argparse
+
+output_file = None
+writer = None
+url = 'http://invhdp01:8080/sax/v1/media/'
+
+def getwriter(header):
+    global output_file
+    global writer
+    if writer == None:
+        if not params.output_filename:
+            fname_out = params.input_filename+'.out'
+        else:
+            fname_out = params.output_filename
+        output_file = open(fname_out,'w+')
+        writer = csv.DictWriter(output_file,fieldnames=header,delimiter='\t')
+        writer.writeheader()
+    return writer
+# Choose method
+def getmethod(line):
+    if 'method' in line:
+        return line['method']
+    return 'all'
+
+def argparser():
+    parser = argparse.ArgumentParser(description='Specify input and output filenames')
+    parser.add_argument('-i', '--input', dest='input_filename', default=None, required=True, help='Full path to input filename')
+    parser.add_argument('-o', '--output', dest='output_filename', default=None, required=False,  help='Full path to output filename')
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    fname = params.input_filename
+    with open(fname,'rb') as f:
+        reader = csv.DictReader(f,delimiter='\t')
+        for line in reader:
+            request = url+getmethod(line)+'?'+urllib.urlencode(line)
+    #        print request
+            response = urllib2.urlopen(request).read()
+            io = StringIO(response)
+            parsed = json.load(io)
+            writer = getwriter(reader.fieldnames+sorted(parsed.keys()))
+            writer.writerow(dict(line.items()+parsed.items()))
+    output_file.close()
+
+params = argparser()
+
+main()
