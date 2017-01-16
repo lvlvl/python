15a3518ee85078d5342a7b4ca5712d9b73b2fe39
Sergey <Sergey@Sergeys-MacBook-Pro-2.local>
2017-01-16 14:51:39 +0200

    gUnZipFile function fix

--- uncompression_component.py
+++ uncompression_component.py
@@ -12,6 +12,7 @@ from subprocess import check_call
 from os import listdir
 from os.path import isfile, join
 from shutil import copyfile
+import glob
 
 def argparser():
     parser = argparse.ArgumentParser(description='Specify parameters')
@@ -24,8 +25,59 @@ def argparser():
     return args
 
 def gUnZipFIle(fullFIlePath):
-    check_call(['gunzip', fullFilePath])
+    zipFile = gzip.open(fullFIlePath,"rb")
+    unCompressedFile = open('/Users/Sergey/Work/output/sample_compress/sample_compress.data',"wb")
+    decoded = zipFile.read()
+    unCompressedFile.write(decoded)
+    zipFile.close()
+    unCompressedFile.close()
 
+
+
+def transform_file(input_folder):
+    os.chdir(input_folder)
+    for file in glob.glob("*.data.*"):
+        print("Data file found: " + file)
+
+#    with open(input_fname,'rb') as f:
+#        #Skip headers from original file
+#        h = int(params.skip)
+#        [f.readline() for x in xrange(h)]
+#
+#        reader = csv.DictReader(f,delimiter=params.delimiter)
+#        headers = reader.fieldnames
+#        output_file = gzip.open(params.temp_folder + fname + '.data.gz','w+')
+#        writer = csv.writer(output_file,delimiter=params.delimiter)
+#
+#        for column in compressed_columns:
+#            dictionaries[column]={}
+#            counters[column]=0
+#
+#        for line in reader:
+#            outrow = list()
+#            for header in headers:
+#    # fill dictionary
+#                if header in compressed_columns:
+#                    if line[header] not in dictionaries[header]:
+#                        dictionaries[header][line[header]]=counters[header]
+#                        counters[header] = counters[header]+1
+#                    outrow.append(dictionaries[header][line[header]])
+#                else:
+#                    outrow.append(line[header])
+#            writer.writerow(outrow)
+#        output_file.close()
+#        if params.verbose:
+#            print('---------------------')
+#            print('Dictionaries created: ')
+#            print('---------------------')
+#        for header in compressed_columns:
+#            dict_name = params.temp_folder + fname + '.map.'+header
+#            if params.verbose:
+#                print(dict_name)
+#            with open(dict_name, 'wb') as handle:
+#                pickle.dump(dictionaries[header], handle, protocol=pickle.HIGHEST_PROTOCOL)
+#                handle.close()
+#                gZipFile(dict_name)
 
 
 params = argparser()
@@ -41,3 +93,6 @@ if params.verbose:
     print('')
     print('Files: ')
     print(onlyfiles)
+
+
+transform_file('/Users/Sergey/Work/output/sample_compress/')
